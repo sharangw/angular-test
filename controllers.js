c603426e833ec3550a8da1163576aefc1989b4ca
Wakankar <sharang.wakankar@capgemini.com>
2017-08-31 10:20:42 -0500

    adding count functionality

--- controllers.js
+++ controllers.js
@@ -8,4 +8,10 @@ angular.module("AppModule").controller("AboutController", function() {
     
     var self = this;
 
+    self.count = 0;
+
+    self.addOne = function() {
+        self.count++;
+    }
+
 });
